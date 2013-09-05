de0d176087c1c782955fc7e071dec102ddd9423e
Linas Valiukas <shirshegsm@gmail.com>
2013-09-05 16:20:06 +0300

    updated sample client

--- sample_client.pl
+++ sample_client.pl
@@ -22,30 +22,33 @@ sub main()
 	# $result = NinetyNineBottlesOfBeer->run_on_gearman();
 	# $result = NinetyNineBottlesOfBeer->run_on_gearman({how_many_bottles => 3});
 	# $result = NinetyNineBottlesOfBeer->enqueue_on_gearman();
-	$result = NinetyNineBottlesOfBeer->enqueue_on_gearman({how_many_bottles => 2000});
+	# $result = NinetyNineBottlesOfBeer->enqueue_on_gearman({how_many_bottles => 2000});
 	# $result = NinetyNineBottlesOfBeer->run_on_gearman({how_many_bottles => 3, 'this' => 'that', 'hacky hack' => '/etc/passwd'});
 
 	# $result = Addition->run_on_gearman({a => 2, b => 3});
 
 	# $result = AdditionAlwaysFails->run_on_gearman({a => 2, b => 3});
+	$result = AdditionAlwaysFails->enqueue_on_gearman({a => 2, b => 3});
 
 	say STDERR "Result: $result";
 
-	my $status;
+	sleep (10);
 
-	sleep(1);
-	$status = GJS->get_gearman_status($result);
-	say STDERR "Status: " . Dumper($status);
+	# my $status;
 
-	GJS->cancel_gearman_job($result);
+	# sleep(1);
+	# $status = GJS->get_gearman_status($result);
+	# say STDERR "Status: " . Dumper($status);
 
-	sleep(3);
-	$status = GJS->get_gearman_status($result);
-	say STDERR "Status: " . Dumper($status);
+	# # GJS->cancel_gearman_job($result);
 
-	sleep(5);
-	$status = GJS->get_gearman_status($result);
-	say STDERR "Status: " . Dumper($status);
+	# sleep(3);
+	# $status = GJS->get_gearman_status($result);
+	# say STDERR "Status: " . Dumper($status);
+
+	# sleep(5);
+	# $status = GJS->get_gearman_status($result);
+	# say STDERR "Status: " . Dumper($status);
 }
 
 main();
